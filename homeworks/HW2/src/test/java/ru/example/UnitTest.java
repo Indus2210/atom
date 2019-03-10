6a608162626ee624c78ebe15f2f5aeb27c00567d
indus <indus1996@yandex.ru>
2019-03-10 23:14:01 +0300

    Добавлены тесты и логгеры

---
+++ homeworks/HW2/src/test/java/ru/example/UnitTest.java
@@ -0,0 +1,39 @@
+package ru.example;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+public class UnitTest {
+
+    @Test
+    public void CheckBulls() throws Exception {
+        String game_word = "aaasss";
+        String player_word = "asasas";
+        int bulls = Game.countBulls(game_word,player_word);
+        assertEquals(4, bulls);
+    }
+
+
+    @Test
+    public void CheckCows() throws Exception {
+        String game_word = "aaasss";
+        String player_word = "asasas";
+        int cows = Game.countCows(game_word,player_word);
+        assertEquals(2, cows);
+    }
+
+    @Test
+    public void CheckReading() throws Exception {
+        String word = Game.WordFromFile();
+        boolean flag;
+        if (word instanceof String)
+        {
+            flag = true;
+        }
+        else {
+            flag = false;
+        }
+        assertTrue(flag);
+    }
+}
