1b0de47baef1594352e5633a03d25c46d8530189
indus <indus1996@yandex.ru>
2019-03-10 21:47:09 +0300

    Доделал игру

--- homeworks/HW2/src/main/java/ru/example/Game.java
+++ homeworks/HW2/src/main/java/ru/example/Game.java
@@ -13,7 +13,7 @@ public class Game {
         StartScrin();
     }
 
-    static void StartScrin(){
+    private static void StartScrin(){
         System.out.println("Выбери что ты хочешь сделать:");
         System.out.println("1 - Начать игру");
         System.out.println("2 - Правила игры");
@@ -34,31 +34,72 @@ public class Game {
 
     }
 
-    static void Exit(){
+    private static void Exit(){
         System.out.println("Спасибо, что поиграл!");
         System.exit(0);
 
     }
 
-    static void Rules(){
+    private static void Rules(){
         System.out.println("Компьетер задумывает слово и объявляет длину слова заранее.");
         System.out.println("Участник предлагают свои варианты слов, а компьютер им отвечает в таком виде (допустим):\n 2б1к - это значит (два быка и одна корова), т.е. две буквы угаданы и стоят на своем месте, одна угадана, но стоит не на своем месте.");
         System.out.println("На угадывание слова дается 5 попыток");
         Scanner in = new Scanner(System.in);
         System.out.println("Для продолжения нажми Enter...");
-        String num = in.nextLine();
+        in.nextLine();
         StartScrin();
 
     }
 
-    public static String removeCharAt(String s, int pos) {
-        return s.substring(0, pos) + s.substring(pos + 1);
+
+    public static int countCows(String wordPlayer, String wordGame) {
+        char letter;
+        int count = 0;
+        boolean[] flagGame;
+        boolean[] flagPlayer;
+
+        flagGame = new boolean[wordGame.length()];
+        flagPlayer = new boolean[wordPlayer.length()];
+        for (int i = 0; i < wordGame.length(); i++) {
+            if (wordGame.charAt(i) == wordPlayer.charAt(i)) {
+                flagGame[i] = false;
+                flagPlayer[i] = false;
+            } else {
+                flagGame[i] = true;
+                flagPlayer[i] = true;
+            }
+
+        }
+        for (int i = 0; i < wordPlayer.length(); i++) {
+            letter = wordPlayer.charAt(i);
+            if (wordGame.indexOf(letter) != -1 && flagPlayer[i]) {
+                for (int j = 0; j < wordGame.length(); j++) {
+                    if (letter == wordGame.charAt(j) && flagGame[j]) {
+                        flagPlayer[i] = false;
+                        flagGame[j] = false;
+                        count++;
+                        break;
+                    }
+                }
+            }
+        }
+        return count;
     }
 
-    static void Game(){
+    public static int countBulls(String wordPlayer, String wordGame) {
+        int count = 0;
+        for (int i = 0; i < wordGame.length(); i++) {
+            if (wordGame.charAt(i) == wordPlayer.charAt(i)) {
+                count++;
+            }
+        }
+        return  count;
+    }
+
+    private static void Game(){
         System.out.println("Игра началась");
         String game_word = WordFromFile();
-        int lenght = game_word.length();
+        int Bulls, Cows;
         System.out.println(game_word);
         System.out.println("Слово состоит из " + game_word.length() + " букв");
         for(int round=1;round<=5;round++) {
@@ -66,23 +107,12 @@ public class Game {
             System.out.println("Введи слово:");
             Scanner in = new Scanner(System.in);
             String num = in.nextLine();
-            int Bulls = 0;
-            int Cows = 0;
-            for (int i = 0; i < game_word.length(); i++) {
-                if (game_word.charAt(i) == num.charAt(i)) {
-                    Bulls++;
-                }
-            }
-            System.out.println(game_word);
-            for (int i = 0; i < game_word.length(); i++)
-                for (int j = 0; j < num.length(); j++)
-                    if (game_word.charAt(i) == num.charAt(j) && i != j){
-                        Cows++;
-                    }
+            Bulls = countBulls(num, game_word);
+            Cows = countCows(num, game_word);
 
             System.out.println("Быков - " + Bulls);
             System.out.println("Коров - " + Cows);
-            if(Bulls == lenght){
+            if(Bulls == game_word.length()){
                 System.out.println("Ты выйграл! Поздравляю!!!!");
                 EndGame();
             }
@@ -92,7 +122,7 @@ public class Game {
         EndGame();
     }
 
-    static void EndGame(){
+    private static void EndGame(){
         System.out.println("Если хочешь сыргать еще жми 1, если хочешь выйти - 2");
         Scanner in = new Scanner(System.in);
         int num = in.nextInt();
